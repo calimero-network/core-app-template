87f7f858be03c593023050d54ee6f8927b7d2b81
matej.vukosav <matej.vukosav@gmail.com>
2024-06-04 15:24:29 +0200

    chore: init with gh workflow

---
+++ app/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+import EnvironmentPlugin from "vite-plugin-environment";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/",
+  build: {
+    outDir: "build",
+  },
+  plugins: [nodePolyfills(), EnvironmentPlugin("all")],
+});
