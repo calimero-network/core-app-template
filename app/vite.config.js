4b9de81d5819cce278b5583a0a905c750d8138b5
matej.vukosav <matej.vukosav@gmail.com>
2024-06-14 17:11:43 +0200

    chore: add homepage for gh pages

--- app/vite.config.js
+++ app/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/core-app-template/',
   build: {
     outDir: 'build',
   },
