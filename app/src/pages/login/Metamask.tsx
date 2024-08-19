154b7a77de8cba00c60ff4fe6668c4856da39c4b
matej.vukosav <matej.vukosav@gmail.com>
2024-08-19 21:56:23 +0200

    fix: use context id instead of app id

--- app/src/pages/login/Metamask.tsx
+++ app/src/pages/login/Metamask.tsx
@@ -3,14 +3,14 @@ import { useNavigate } from 'react-router-dom';
 import { getNodeUrl } from '../../utils/node';
 import { MetamaskWrapper } from '@calimero-is-near/calimero-p2p-sdk';
 import ContentWrapper from '../../components/login/ContentWrapper';
-import { getApplicationId } from '../../utils/env';
+import { getContextId } from '../../utils/env';
 
 export default function MetamaskPage() {
   const navigate = useNavigate();
   return (
     <ContentWrapper>
       <MetamaskWrapper
-        applicationId={getApplicationId()}
+        contextId={getContextId()}
         rpcBaseUrl={getNodeUrl()}
         successRedirect={() => navigate('/home')}
         navigateBack={() => navigate('/')}
