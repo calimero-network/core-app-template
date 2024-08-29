8efb539ec4d68a8a0864061d5b5553b64659d6ae
Fran Domovic <fran@calimero.network>
2024-08-29 12:59:17 +0200

    fix: lint

--- app/src/pages/setup/index.tsx
+++ app/src/pages/setup/index.tsx
@@ -3,7 +3,10 @@ import { useNavigate } from 'react-router-dom';
 import { SetupModal } from '@calimero-is-near/calimero-p2p-sdk';
 import ContentWrapper from '../../components/login/ContentWrapper';
 import { getNodeUrl, getStorageApplicationId } from '../../utils/node';
-import { setStorageAppEndpointKey, setStorageApplicationId } from '../../utils/storage';
+import {
+  setStorageAppEndpointKey,
+  setStorageApplicationId,
+} from '../../utils/storage';
 
 export default function SetupPage() {
   const navigate = useNavigate();
