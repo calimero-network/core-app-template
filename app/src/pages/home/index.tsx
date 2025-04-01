3e4a566c42ff5214a00bdb435ef240b3d16b2fb7
Fran Domovic <fran@calimero.network>
2025-04-01 13:09:13 +0200

    fix: cleanup login

--- app/src/pages/home/index.tsx
+++ app/src/pages/home/index.tsx
@@ -140,7 +140,9 @@ export default function HomePage() {
   }
 
   useEffect(() => {
-    getCount();
+    if (accessToken) {
+      getCount();
+    }
   }, []);
 
   const observeNodeEvents = async () => {
